9b7a59515b912d852e51296830be3bb4e8ee83fc
‘Shirahata777’ <y_shirahata_181@yahoo.co.jp>
2021-07-07 02:24:55 +0900

    StringBuilderを用いた文字列連結

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 //		Practice1.practice();
 //		Practice2.practice();
-		Practice3.practice();
+//		Practice3.practice();
+		Practice4.practice();
 	}
 
 }
