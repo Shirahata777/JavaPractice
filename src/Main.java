1bbc6c542b6551c8c82d3a7b754211814bf6e1a4
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-07-21 23:32:18 +0900

    コード追加

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import  pkg.practice.*;
+import pkg.practice.chapter1.*;
 public class Main {
 
 	public static void main(String[] args) {
@@ -6,9 +7,10 @@ public class Main {
 //		Practice2.practice();
 //		Practice3.practice();
 //		Practice4.practice();
-		String name = "";
-		System.out.println(Practice5.isValidPlayerName1(name));
-		System.out.println(Practice5.isValidPlayerName2(name));
+//		String name = "";
+//		System.out.println(Practice5.isValidPlayerName1(name));
+//		System.out.println(Practice5.isValidPlayerName2(name));
+//		Practice2_1.practice();
 		
 	}
 
