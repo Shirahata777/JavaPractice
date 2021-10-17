a9daca60ed6bcd27c122c9b27f02ddd76ba873f0
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-10-17 14:40:26 +0900

    可変調引数に関して

---
+++ src/pkg/javasilver/chapter6/Practice7.java
@@ -0,0 +1,26 @@
+package pkg.javasilver.chapter6;
+
+public class Practice7 {
+
+//	正解
+	void method(int... a) {
+
+	}
+
+//	可変調引数の定義が違う
+//	void method(int a...) {
+//
+//	}
+
+//	可変調定数は最後に指定しなくてはいけない
+//	void method(int... a, int... b ) {
+//
+//	}
+	
+//	そもそも引数としてvoidを受け取らない
+//	void method(void) {
+//
+//	}
+
+
+}
