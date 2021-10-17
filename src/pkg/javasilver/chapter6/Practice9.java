c193a428f65d53e019b74800c1e87c14c3e308e0
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-10-17 17:57:29 +0900

    オーバーロードについて

---
+++ src/pkg/javasilver/chapter6/Practice9.java
@@ -0,0 +1,22 @@
+package pkg.javasilver.chapter6;
+
+public class Practice9 {
+	
+	void method (double a) {
+	}
+	
+	// オーバーロード
+	void method (int a) {
+	}
+	// オーバーロード
+	void method (String a) {
+	}
+	
+	// アクセス修飾子が異なる　→　オーバーロードではない
+//	private void method () {
+//	}
+	// アクセス修飾子が異なる　→　オーバーロードではない
+//	public void method () {
+//	}
+
+}
