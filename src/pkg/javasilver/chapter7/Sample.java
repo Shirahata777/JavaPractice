061796cb1eb0e1d037cfb7d6a820ac76af52271a
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-10-31 17:51:17 +0900

    インターフェース内のメソッドについて

--- src/pkg/javasilver/chapter7/Sample.java
+++ src/pkg/javasilver/chapter7/Sample.java
@@ -2,5 +2,15 @@ package pkg.javasilver.chapter7;
 
 // public を外してもpublicに変換される
 interface Sample {
+	// インターフェース内のメソッドはstaticかdefaault修飾子をつけないと記述できない
+//	public void test() {
+//		
+//	}
+	default void test1() {
 
+	}
+
+	public static void test2() {
+
+	}
 }
