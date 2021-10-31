262cc63cbae1aaf503d587fb62976f823368b07b
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-10-31 16:14:20 +0900

    コンストラクタをthisで呼び出す場合

--- src/pkg/javasilver/chapter6/Sample.java
+++ src/pkg/javasilver/chapter6/Sample.java
@@ -3,7 +3,10 @@ package pkg.javasilver.chapter6;
 public class Sample {
 	
 	public Sample() {
+//		thisの前に処理はかけない
+//		System.out.println("A");
 		this(null, 0);
+		System.out.println("A");
 	}
 
 	public Sample(Object object, int i) {
