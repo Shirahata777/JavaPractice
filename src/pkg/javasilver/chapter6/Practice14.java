c19ca5d8c6025bdb6b8d6f25b9f48354abc35642
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-10-31 16:12:02 +0900

    コンストラクタをthisで呼び出す場合

---
+++ src/pkg/javasilver/chapter6/Practice14.java
@@ -0,0 +1,8 @@
+package pkg.javasilver.chapter6;
+
+public class Practice14 {
+
+	public static void main(String[] args) {
+		Sample s = new Sample();
+	}
+}
---
+++ src/pkg/javasilver/chapter6/Sample.java
@@ -0,0 +1,13 @@
+package pkg.javasilver.chapter6;
+
+public class Sample {
+	
+	public Sample() {
+		this(null, 0);
+	}
+
+	public Sample(Object object, int i) {
+		System.out.println("OK!");
+	}
+
+}
