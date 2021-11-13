8327266fdd4ae445d916ea95c09cd80b6b60a444
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-11-13 18:47:37 +0900

    Math API について

---
+++ src/pkg/practice/chapter9/Practice1.java
@@ -0,0 +1,11 @@
+package pkg.practice.chapter9;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+//		平方根
+		System.out.println(Math.sqrt(2));
+//		累乗
+		System.out.println(Math.pow(2, 3));
+	}
+}
