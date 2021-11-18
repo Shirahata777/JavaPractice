14f26e6d210e5d36fd16ca6497f077bfe868d843
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-11-18 20:08:21 +0900

    並び替えAPI（Comparatorについて）

--- src/pkg/practice/chapter9/Practice3.java
+++ src/pkg/practice/chapter9/Practice3.java
@@ -9,7 +9,14 @@ public class Practice3 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-		list.sort((a, b) -> -a.compareTo(b));
+//		デフォルトは自然順
+		list.sort((a, b) -> a.compareTo(b));
+		
+//		-つけると逆になる
+//		list.sort((a, b) -> -a.compareTo(b));
+		
+//		変数を左右入れ替えても逆順になる
+//		list.sort((a, b) -> b.compareTo(a));
 
 		for (Integer i : list) {
 			System.out.println(i);
