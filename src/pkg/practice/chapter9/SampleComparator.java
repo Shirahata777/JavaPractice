4f813a9f3b5ee4a6700817254e7931f39f98aafa
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-11-18 19:57:31 +0900

    コメント追加

--- src/pkg/practice/chapter9/SampleComparator.java
+++ src/pkg/practice/chapter9/SampleComparator.java
@@ -2,6 +2,8 @@ package pkg.practice.chapter9;
 
 import java.util.Comparator;
 
+//Comparatorは retrun 1 を返すと入れ替え、retrun -1 だと入れ替えない
+//ファイルを分けずに並び替えを行うクラスに実装するには、compareToメソッドを実装する
 public class SampleComparator implements Comparator<Sample2> {
 	@Override
 	public int compare(Sample2 s1, Sample2 s2) {
