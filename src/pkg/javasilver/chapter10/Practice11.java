accb3c23cb2bea5f174cacb0472c739883fd005f
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-11-28 17:08:30 +0900

    検査例外と非検査例外

--- src/pkg/javasilver/chapter10/Practice11.java
+++ src/pkg/javasilver/chapter10/Practice11.java
@@ -3,7 +3,7 @@ package pkg.javasilver.chapter10;
 public class Practice11 {
 
 //	throw宣言を記載
-//	TestExceptionはなくてもよい
+//	TestExceptionはなくてもよい（RuntimeExceptionは非検査例外のため）
 	public void hello(String name) throws SampleException {
 		if (name == null) {
 			throw new SampleException();
