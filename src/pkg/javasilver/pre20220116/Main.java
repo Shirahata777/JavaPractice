38c46b1758a94b20396114be7083682d7d92e215
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2022-01-16 17:26:38 +0900

    20220116日分

--- src/pkg/javasilver/pre20220116/Main.java
+++ src/pkg/javasilver/pre20220116/Main.java
@@ -2,10 +2,17 @@ package pkg.javasilver.pre20220116;
 
 public class Main {
 	
+	static int num = 10;
+	
 	public static void main(String[] args) {
 //		配列の要素数が指定されている時、{}による変数代入は使えない
 //		String[] list = new String[3]{};
 //		System.out.println(list);
+		
+		Main.num += 10;
+		Main m = new Main();
+//		コンパイル時に「Main.num = 30」に置き換わる
+		m.num = 30;
 	}
 
 }
