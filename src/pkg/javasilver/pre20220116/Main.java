0c1516a1c324a480720627acd77282686e3af5c0
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2022-01-16 17:04:51 +0900

    20220116日分

---
+++ src/pkg/javasilver/pre20220116/Main.java
@@ -0,0 +1,11 @@
+package pkg.javasilver.pre20220116;
+
+public class Main {
+	
+	public static void main(String[] args) {
+//		配列の要素数が指定されている時、{}による変数代入は使えない
+//		String[] list = new String[3]{};
+//		System.out.println(list);
+	}
+
+}
